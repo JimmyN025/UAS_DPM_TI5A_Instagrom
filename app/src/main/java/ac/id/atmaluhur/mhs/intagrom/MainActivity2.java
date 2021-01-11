d0906e8fb529398e504ce7328af1c7d960a0bc5e
JimmyN025 <76139887+JimmyN025@users.noreply.github.com>
2021-01-11 12:11:07 +0700

    Add files via upload

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/intagrom/MainActivity2.java
@@ -0,0 +1,32 @@
+package ac.id.atmaluhur.mhs.intagrom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        getSupportActionBar().setTitle("Home");
+    }
+}
+
+
+
+
